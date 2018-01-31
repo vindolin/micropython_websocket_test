7492bbecc5f2d814ba26e66675c4c74eaee405da
Thomas Schüßler <vindolin@gmail.com>
2018-01-31 18:06:01 +0100

    WIP

--- wifi.py
+++ wifi.py
@@ -1,26 +1,30 @@
 import network
+from networks import networks
 from utime import sleep
 WIFI_RETRY_SECONDS = 10
 
 sta_if = network.WLAN(network.STA_IF)
 
 
-def _connect(networks):
+def _connect():
     sta_if.active(True)
 
+    visible_essids = [wifi[0].decode() for wifi in sta_if.scan()]
+
     for essid, password in networks:
-        print('\ntrying {}'.format(essid))
-        sta_if.connect(essid, password)
-        for i in range(WIFI_RETRY_SECONDS):
-            print('.', end='')
-            sleep(1)
-            if sta_if.isconnected():
-                print('\nconnected to {} - {}'.format(essid, sta_if.ifconfig()))
-                return essid, sta_if
+        if essid in visible_essids:
+            print('\ntrying {}'.format(essid))
+            sta_if.connect(essid, password)
+            for i in range(WIFI_RETRY_SECONDS):
+                print('.', end='')
+                sleep(1)
+                if sta_if.isconnected():
+                    print('\nconnected to {} - {}'.format(essid, sta_if.ifconfig()))
+                    return essid, sta_if
 
 
-def connect(networks, sta_callback=None, ap_callback=None, wifi_retry_seconds=WIFI_RETRY_SECONDS):
-    result = _connect(networks)
+def connect_wifi(sta_callback=None, ap_callback=None, wifi_retry_seconds=WIFI_RETRY_SECONDS):
+    result = _connect()
 
     if result:
         if sta_callback:
